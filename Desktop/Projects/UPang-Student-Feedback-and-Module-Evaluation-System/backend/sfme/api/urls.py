8af289c9f5ee4fd66525307abb2df6f96c538fe4
John Rasheed Paner <148771728+endevium@users.noreply.github.com>
2026-02-02 21:47:30 +0800

    Delete Desktop/Projects/UPang-Student-Feedback-and-Module-Evaluation-System/backend/sfme directory

--- Desktop/Projects/UPang-Student-Feedback-and-Module-Evaluation-System/backend/sfme/api/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("students/", views.StudentListCreateView.as_view(), name="student-list-create"),
-    path('students/<int:pk>/', views.StudentDetailView.as_view(), name='student-detail'),
-]
