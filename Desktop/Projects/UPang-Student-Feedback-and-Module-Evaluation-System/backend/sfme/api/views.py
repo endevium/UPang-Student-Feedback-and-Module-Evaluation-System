36cf2758af28bc1a268e7055b7201a2b6d7b3402
John Rasheed Paner <jrasheedpaner@gmail.com>
2026-02-02 21:46:24 +0800

    Initialization

---
+++ Desktop/Projects/UPang-Student-Feedback-and-Module-Evaluation-System/backend/sfme/api/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from rest_framework import generics
+from .models import Student
+from .serializers import StudentSerializer
+
+# Create your views here.
+class StudentListCreateView(generics.ListCreateAPIView):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+
+class StudentDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
